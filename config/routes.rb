05215bcfe9e6b48bed02d8899f49184e190f3136
ASSROUGUI <ashwagooo@hotmail.com>
2019-09-07 18:23:51 +0300

    Heroku config

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+
+
 Rails.application.routes.draw do
   namespace :api do
    namespace :v1 do
@@ -5,5 +7,5 @@ Rails.application.routes.draw do
    end
   end
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- end
- 
+
+end
